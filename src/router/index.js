9fbe6c212b352a72cedb1e1d626887a87e456620
RengbarH <86048645+RengbarH@users.noreply.github.com>
2022-06-30 13:27:30 +0200

    Neue NavBar  + und CI + css-style implementiert

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import AboutView from "@/views/AboutView.vue";
+import JerkView from "@/views/JerkView.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+  { path: "/", name: "home", component: HomeView },
+  { path: "/about", name: "about", component: AboutView },
+  { path: "/jerk", name: "jerk", component: JerkView },
 ];
 
 const router = createRouter({
---
+++ src/views/JerkView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="jerk">
+    <h1>jerk</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JerkView",
+};
+</script>
+
+<style scoped></style>
