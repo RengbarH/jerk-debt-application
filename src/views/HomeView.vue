5d942acd176b3488d07c8404ddf878d9d7109b0d
RengbarH <86048645+RengbarH@users.noreply.github.com>
2022-06-30 18:36:43 +0200

    Backend URL umgebungsabhängig verwenden

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -33,12 +33,14 @@ export default {
     },
   },
   mounted() {
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + "/api/v1/creditor";
+
     const requestOptions = {
       method: "GET",
       redirect: "follow",
     };
 
-    fetch("http://localhost:8080/api/v1/creditor", requestOptions)
+    fetch(endpoint, requestOptions)
       .then((response) => response.json())
       .then((result) =>
         result.forEach((jerk) => {
