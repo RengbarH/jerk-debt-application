635733040e0df21977233b731df07c953899b731
PetermannTobias <saibot.96@gmx.net>
2022-07-02 17:02:39 +0200

    FrontendTest 2 verbessert

--- tests/unit/JerkViewTest.spec.js
+++ tests/unit/JerkViewTest.spec.js
@@ -1,20 +1,20 @@
-import { mount } from '@vue/test-utils'
+import { mount } from "@vue/test-utils";
+import HomeView from "@/views/HomeView";
 import JerkSingle from "@/components/JerkSingle";
 import JerkView from "@/views/JerkView";
-
+import jerkView from "@/views/JerkView";
 
 describe("JerkView.vue", () => {
-    it("should show page title", () => {
-        const wrapper = mount(JerkView)
+  it("should show page title", () => {
+    const wrapper = mount(JerkView);
 
-        expect(wrapper.text()).toMatch('jerk')
+    expect(wrapper.text()).toMatch("jerk");
+  });
 
-    })
+  it("should create from component", () => {
+    const wrapper = mount(JerkView);
 
-    it("should create from component", () => {
-        const wrapper = mount(JerkView)
-
-        const createForm = wrapper.findComponent(JerkSingle)
-        expect(createForm.exists()).toBeTruthy()
-    })
-})
+    const createForm = wrapper.findComponent(JerkSingle);
+    expect(!createForm.exists()).toBeTruthy();
+  });
+});
