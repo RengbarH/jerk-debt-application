04e0b9e7cf687b8c116fc3066b2848b5063eb782
RengbarH <86048645+RengbarH@users.noreply.github.com>
2022-07-04 11:53:42 +0200

    AboutView finalisiert

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <container>
+    <div class="about">
+      <h1 class="h-about">Über das Projekt</h1>
+    </div>
+    <div>
+      <h3>Studierende: Rengbar Hardam (579616) | Tobias Petermann (...)</h3>
+      <div class="destination-details">
+        Du bist sehr nett und leihst Freunden als auch Fremden gerne Geld aus,
+        ohne es irgendwo festzuhalten? Dann bist du hier genau richtig! Wir
+        haben im Rahmen unseres Projekts in Webtechnologien (Sommersemesters
+        2022) eine webbasierte Schuldenliste programmiert. Diese Schuldenliste
+        soll dir helfen, die Schuldner einzutragen und wieder zu löschen.
+      </div>
+      <div class="abouthappy">Have fun!</div>
+    </div>
+  </container>
 </template>
